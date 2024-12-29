dc9bac643e50fa0ee1f71a849466bb13685d9afa
Anm-7 <121972526+Anm-7@users.noreply.github.com>
2024-12-29 22:07:29 +0300

    Update master.js

--- master.js
+++ master.js
@@ -149,6 +149,7 @@ function works() {
       { image: './works-imgs/restaurant.jpg', title: 'تصميم جرافيكي', category: "graphics" },
       { image: './works-imgs/restaurant2.jpg', title: 'تصميم جرافيكي', category: "graphics" },
       { image: './works-imgs/burger.jpg', title: 'تصميم جرافيكي', category: "graphics" },
+      { image: './works-imgs/king burger.jpg', title: 'تصميم جرافيكي', category: "graphics" },
   ];
 
   const detail_list = document.getElementById("detail_list");
